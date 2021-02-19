193823b66d8957ff903f1670bf5bf8709f32911f
Diego <dncoppaelectronica@gmail..com>
2021-02-19 17:40:18 -0300

    xml reader

--- Project010/etree.v1.00.py
+++ Project010/etree.v1.00.py
@@ -4,22 +4,28 @@ Created on Fri Feb 19 09:37:44 2021
 
 @author: Diego
 """
-import xml.etree.ElementTree as ET
+from lxml import etree as ET
 
 
 tree = ET.parse('C:/43Python/01.RepositorioGithub/python_projects/Project010/calibracion.xml')
 
-#root = fromstring(xml_text)
-"""
 
 root = tree.getroot()
 print(root.tag)
 print(root.attrib)
+"""
 for child in root:
     print(child.tag, child.attrib)
-print(root[0][1].text)
+"""
+
+for child in root:
+    print({x.tag for x in root.findall(child.tag+"/*")})
+
+
+
+#print(root[0][1].text)
+"""
 for neighbor in root.iter('neighbor'):
     print(neighbor.attrib)
 #print(root[0][1].text)
-
 """
